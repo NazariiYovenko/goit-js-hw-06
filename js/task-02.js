70642c0d53051a5c76f472300fd679eb160a9b56
NazariiYovenko <nazarjovenko@gmail.com>
2022-12-04 15:03:41 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+let elements = [];
+for (const ingredient of ingredients) {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = ingredient;
+  elements.push(li);
+}
+list.append(...elements);
