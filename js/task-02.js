e5a80cc0f5e910b9cf7909ed4e9b1e381253ba38
NazariiYovenko <nazarjovenko@gmail.com>
2022-12-05 17:45:24 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,11 +8,10 @@ const ingredients = [
 ];
 const list = document.querySelector("#ingredients");
 let elements = [];
-ingredients.map((el) => createLi(el));
-function createLi(ingredient) {
+ingredients.map((ingredient) => {
   const li = document.createElement("li");
   li.classList.add("item");
   li.textContent = ingredient;
   elements.push(li);
-}
+});
 list.append(...elements);
