36c548f3ec73c7fdc982a7c216638d460ee1cbeb
NazariiYovenko <nazarjovenko@gmail.com>
2022-12-05 20:41:35 +0200

    refix task-1

--- js/task-01.js
+++ js/task-01.js
@@ -2,5 +2,5 @@ const items = document.querySelectorAll("#categories .item");
 console.log(`Number of categories: ${items.length}\n\n`);
 [...items].map((item) => {
   console.log(`Category: ${item.firstElementChild.textContent}`);
-  console.log(`Elements: ${item.querySelectorAll("li").length}\n\n`);
+  console.log(`Elements: ${item.lastElementChild.children.length}\n\n`);
 });
