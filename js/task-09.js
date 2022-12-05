5eb0f5020d25e91b4b13e1c2a331619f4ebd2fe2
NazariiYovenko <nazarjovenko@gmail.com>
2022-12-05 13:29:50 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+let colorName = document.querySelector(".color");
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+button.addEventListener("click", () => {
+  colorName.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+});
